1c68e684c5239180ef576ecaa8e25ad75e614f6e
Dzmitry Harlach <dzmitry@m2.com>
2025-03-01 16:52:53 +0300

    GTA-1 Create a project as a multi-module Maven project
    - create three services api-gateway, order-service, inventory-service

---
+++ order-service/src/test/java/com/g42/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.g42.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //context load test
+    }
+
+}
